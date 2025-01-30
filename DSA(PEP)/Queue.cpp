ebdb806223da6da8c369de2f991045f59063095d
nishantsingh1107 <nishantsingh0311@gmail.com>
2025-01-30 11:47:24 +0530

    queqe

---
+++ DSA(PEP)/Queue.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int main(){
+//     queue<int> q;
+//     q.push(1);
+//     q.push(2);
+//     q.push(3);
+
+//     cout<<q.front()<<endl;
+//     cout<<q.size()<<endl;
+
+//     q.pop();
+//     cout<<q.back()<<endl;
+//     cout<<q.front()<<endl;
+//     cout<<q.size()<<endl;
+//     cout<<q.empty()<<endl;
+// }
+
+
+// class Queue {
+//     public:
+//     vector<int> q;
+//     void enqueue(int val){
+//         q.push_back(val);
+//     }
+//     void dequeue(){
+//         if(!q.empty()){
+//             q.erase(q.begin());
+//         } 
+//         else{
+//             cout<<"Queue is empty!"<<endl;
+//         }
+//     }
+//     void printQueue(){
+//         if(q.empty()){
+//             cout<<"Queue is empty!"<<endl;
+//             return;
+//         }
+//         cout<<"Queue elements: ";
+//         for(int val : q){
+//             cout<<val<< " ";
+//         }
+//         cout<<endl;
+//     }
+// };
+
+// int main() {
+//     Queue q;
+
+//     q.enqueue(10);
+//     q.enqueue(20);
+//     q.enqueue(30);
+//     q.enqueue(40);
+//     q.printQueue();
+//     q.dequeue();
+//     q.printQueue();
+//     return 0;
+// }
+
+
+
+void reverse(queue<int>& q){
+	stack<int> st;
+	while (!q.empty()) {
+		st.push(q.front());
+		q.pop();
+	}
+	while (!st.empty()) {
+		q.push(st.top());
+		st.pop();
+	}
+}
+int main(){
+	queue<int> q;
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	q.push(5);
+
+	reverse(q);
+	while (!q.empty()) {
+		cout<<q.front()<<" ";
+		q.pop();
+	}
+}
