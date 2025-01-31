020ad2e6cd2d188f2dc5cdef5619faf0494a4749
nishantsingh1107 <nishantsingh0311@gmail.com>
2025-01-31 11:19:55 +0530

    queue

--- DSA(PEP)/Queue.cpp
+++ DSA(PEP)/Queue.cpp
@@ -60,28 +60,64 @@ using namespace std;
 
 
 
-void reverse(queue<int>& q){
-	stack<int> st;
-	while (!q.empty()) {
-		st.push(q.front());
-		q.pop();
-	}
-	while (!st.empty()) {
-		q.push(st.top());
-		st.pop();
-	}
-}
-int main(){
-	queue<int> q;
-	q.push(1);
-	q.push(2);
-	q.push(3);
-	q.push(4);
-	q.push(5);
+// void reverse(queue<int>& q){
+// 	stack<int> st;
+// 	while (!q.empty()) {
+// 		st.push(q.front());
+// 		q.pop();
+// 	}
+// 	while (!st.empty()) {
+// 		q.push(st.top());
+// 		st.pop();
+// 	}
+// }
+// int main(){
+// 	queue<int> q;
+// 	q.push(1);
+// 	q.push(2);
+// 	q.push(3);
+// 	q.push(4);
+// 	q.push(5);
 
-	reverse(q);
-	while (!q.empty()) {
-		cout<<q.front()<<" ";
-		q.pop();
-	}
+// 	reverse(q);
+// 	while (!q.empty()) {
+// 		cout<<q.front()<<" ";
+// 		q.pop();
+// 	}
+// }
+
+
+
+void reverseKelements(queue<int>& q, int k){
+    stack<int> s;
+    for(int i=0;i<k;i++){
+        s.push(q.front());
+        q.pop();
+    }
+    while(!s.empty()){
+        q.push(s.top());
+        s.pop();
+    }
+	for(int i=0;i<q.size()-k;i++){
+        q.push(q.front());
+        q.pop();
+    }
+}
+
+int main(){
+    queue<int> q;
+    q.push(6);
+    q.push(10);
+    q.push(8);
+    q.push(12);
+    q.push(3);
+	
+    reverseKelements(q,2);
+
+    while(!q.empty()){
+        cout<<q.front()<<" ";
+        q.pop();
+    }
+    cout<<endl;
+    return 0;
 }
